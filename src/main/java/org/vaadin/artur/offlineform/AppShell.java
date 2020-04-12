93e90ebaaf3a5bcb1786c5f3bb3ba12ce6af1402
Artur Signell <artur@vaadin.com>
2020-04-12 13:58:32 +0300

    Use experimental Flow with built in offline support

--- src/main/java/org/vaadin/artur/offlineform/AppShell.java
+++ src/main/java/org/vaadin/artur/offlineform/AppShell.java
@@ -1,6 +1,9 @@
 package org.vaadin.artur.offlineform;
 
 import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.server.PWA;
 
+@PWA(name = "Offline Form", shortName = "Offline Form", offlineResources = { "*.*",
+        "icons/**" }, enableInstallPrompt = false)
 public class AppShell implements AppShellConfigurator {
 }
